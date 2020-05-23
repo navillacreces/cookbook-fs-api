6357c60b757bcb1727f03281ef974912d1c7152b
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-05-22 19:35:58 -0500

    recipeRouter

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,18 +1,48 @@
 const express = require('express')
-
+const recipeService = require('./recipesService')
 const recipeRoute = express.Router()
 const bodyParser = express.json()
 
 
 recipeRoute
-    .route('/recipes')
-
+    .route('/')
 
     .get((req,res,next) =>{
 
+        const knexInstance = req.app.get('db')
+
+        recipeService.getAllRecipes(knexInstance)
+            .then(recipes =>{
+                res.json(recipes)
+            })
+            .catch(next)
+        
     })
 
-
     .post(bodyParser,(req,res) =>{
+
+        const {name,cooktime,servings,ingredients,instructions} = req.body;
+
+        if (!name || !cooktime || !servings || !ingredients || !ingredients){
+            return res.status(400).send('invalid data')
+        }
+
+        const newRecipe = {
+            name: name,
+            cooktime: cooktime,
+            servings: servings,
+            ingredients: ingredients,
+            instructions: instructions
+        }
+
+        const knexInstance = req.app.get('db')
+
+        recipeService.insertRecipe(knexInstance,newRecipe)
+            .then(recipe =>{
+                res.location(`https://localhost:8000/recipes/{recipe.id}`).status(201).json(recipe)
+
+            })
+            .catch(next)
+        
 
     })
