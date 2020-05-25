6911de2f8e8402db13cbde207265ee47f4125c9e
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-05-24 20:31:38 -0500

    debug

--- src/server.js
+++ src/server.js
@@ -8,8 +8,7 @@ const db = knex({
 })
 
 app.set('db',db)
-/*
+
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+//  console.log(`Server listening at http://localhost:${PORT}`)
 })
-*/
